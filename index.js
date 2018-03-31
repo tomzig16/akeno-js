fe6b011815149201983a03ee5ae99ed6e20239b0
Tomas Zigmantavičius <tzigmantavicius@gmail.com>
2018-03-31 19:12:21 +0300

    Init ping replying bot

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 require('dotenv').config();
 var mysql = require('mysql');
+const Discord = require('discord.js');
+
+const client = Discord.Client();
+const token = process.env.BOT_TOKEN;
+client.login(token);
 
 var con = mysql.createConnection({
   host: process.env.DB_HOST,
@@ -9,5 +14,15 @@ var con = mysql.createConnection({
 
 con.connect(function(err) {
   if (err) throw err;
-  console.log("Connected!");
+  console.log("Connected to database!");
+});
+
+client.on('ready', function() {
+  console.log('Bot has been started');
+});
+
+client.on('message', message => {
+  if (message.content === 'ping') {
+    message.channel.send('pong');
+  }
 });
