dce3389bf1fe1812d1b7c1a9fd8e54a39607ce0e
Tomas Zigmantavičius <tzigmantavicius@gmail.com>
2018-04-01 13:42:49 +0300

    Fix OnBotRemoved from server function

--- server_control.js
+++ server_control.js
@@ -3,9 +3,9 @@ module.exports = {
     console.log(`New guild joined: ${serverData.name} (id: ${serverData.id}). This guild has ${serverData.memberCount} members!`);
     botClient.user.setGame(`on ${botClient.guilds.size} servers`);
   },
-  OnBotRemoved: function(serverData){
-    console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
-    client.user.setGame(`on ${client.guilds.size} servers`);
+  OnBotRemoved: function(botClient, serverData){
+    console.log(`I have been removed from: ${serverData.name} (id: ${serverData.id})`);
+    botClient.user.setGame(`on ${botClient.guilds.size} servers`);
   },
   OnAddUserCMD: function(){
     console.log("Attempting to add user");
