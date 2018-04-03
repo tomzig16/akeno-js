b5149c34c787c280af7b90b4a62bd6526e143593
Tomas Zigmantavičius <tzigmantavicius@gmail.com>
2018-04-03 23:20:17 +0300

    Add !addserver command

--- scripts/bot_commands.js
+++ scripts/bot_commands.js
@@ -4,7 +4,7 @@ module.exports = {
     dbConnection.query(sql, function (err, result, fields) {
       if (err) throw err;
       if(result == ""){
-        InsertServer(server.id, dbConnection);
+        InsertServer(server, dbConnection);
       }
       else{
         console.log("Server already exists in database.");
@@ -13,20 +13,43 @@ module.exports = {
   }
 };
 
-function InsertServer(serverID, dbConnection){
+function InsertServer(server, dbConnection){
   var sqlInsertServConf =  "INSERT INTO `server_conf` (`id`, `admin_role`) VALUES (NULL, NULL);";
   dbConnection.query(sqlInsertServConf, function (err_ins_conf, result_ins_conf) {
     if (err_ins_conf) throw err_ins_conf;
+
     var sqlInsertServer = "INSERT INTO `servers` (`id`, `dscr_id`, `admin_fk`, `server_conf_fk`) VALUES (NULL, '" + 
-                          serverID + "', NULL, '" + result_ins_conf.insertId + "');";
+                          server.id + "', NULL, '" + result_ins_conf.insertId + "');";
+
     dbConnection.query(sqlInsertServer, function(err_ins_server, result_ins_server){
       if(err_ins_server) throw err_ins_server;
-      console.log("Inserted into servers");
+      console.log("Server was added to database.");
+
+      InsertServerOwner(server.owner, result_ins_server.insertId, dbConnection);
     });
   });
 
-          /*
-        INSERT INTO `akeno_debug`.`users` (`id`, `name`, `dscr_id`, `server_fk`) VALUES (NULL, 'Akeno', '429672169203695616', '1');
-        INSERT INTO `akeno_debug`.`user_stats` (`id`, `user_fk`, `honored`, `spare_honors`) VALUES (NULL, '1', '0', '15');
-        */
+  function InsertServerOwner(serverOwner, serverTableID, dbConnection){
+    var sqlInsertServConf =  "INSERT INTO `users` (`id`, `name`, `dscr_id`, `server_fk`) VALUES " +
+    "(NULL, '" + serverOwner.user.tag + "', '" + serverOwner.id + "', '" + serverTableID + "');";
+    dbConnection.query(sqlInsertServConf, function (err_ins_usr, result_ins_usr) {
+      if (err_ins_usr) throw err_ins_usr;
+      console.log("Main admin was added to DB.");
+
+      // Update server admin fk
+      var sqlUpdateServ = "UPDATE `servers` SET `admin_fk` = '" + result_ins_usr.insertId + "' WHERE `servers`.`id` = " + serverTableID;
+      dbConnection.query(sqlUpdateServ, function (err_upd_serv, result_upd_serv) {
+        if (err_upd_serv) throw err_upd_serv;
+        console.log(result_upd_serv.affectedRows + " record(s) updated");
+      });
+
+      // Insert into user_stats table
+      var sqlInsertUStats = "INSERT INTO `user_stats` (`id`, `user_fk`, `honored`, `spare_honors`) " +
+      "VALUES (NULL, '" + result_ins_usr.insertId + "', '0', '15');";
+      dbConnection.query(sqlInsertUStats, function (err_ins_ustats, result_ins_ustats) {
+        if (err_ins_ustats) throw err_ins_ustats;
+        console.log("User stats row was added to DB.");
+      });
+    });
+  }
 }
