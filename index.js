d222b890851e5a6dc62b91544c2a7204899cac59
Tomas Zigmantavičius <tzigmantavicius@gmail.com>
2018-04-06 00:17:54 +0300

    Add !join_h command

--- index.js
+++ index.js
@@ -55,6 +55,23 @@ client.on("message", async message => {
       return message.reply("Sorry, you don't have permission to do that.");
     }
   }
+
+  if(command === "join_h"){
+    serverControl.OnAddUser(message.guild.id, message.author.id, con, function(result){
+      if(result === "User already exists"){
+        message.reply("seems like you already exist...");
+      }
+      else if(result === "Server not found"){
+        message.reply("oof! This server does not exist in my memory.");
+      }
+      else if(result === "OK"){
+        message.reply("Yay! I have added you to my database!\nYou can check your status with !status command");
+      }
+      else{
+        console.log("Unknown callback message?");
+      }
+    });
+  }
 });
 
 
--- scripts/server_interactions.js
+++ scripts/server_interactions.js
@@ -3,21 +3,44 @@ module.exports = {
     console.log(`New guild joined: ${serverData.name} (id: ${serverData.id}). This guild has ${serverData.memberCount} members!`);
     botClient.user.setActivity(`over ${botClient.guilds.size} servers`, { type: "WATCHING" } );
   },
+
   OnBotRemoved: function(botClient, serverData){
     console.log(`I have been removed from: ${serverData.name} (id: ${serverData.id})`);
     botClient.user.setActivity(`over ${botClient.guilds.size} servers`, { type: "WATCHING" });
   },
-  OnAddServer: function(server, dbConnection, actionResult){
+
+  OnAddServer: function(server, dbConnection, ResultCallback){
     var sql = "SELECT * FROM `servers` WHERE `dscr_id` = " + server.id;
     dbConnection.query(sql, function (err, result, fields) {
       if (err) throw err;
       if(result == ""){
         InsertServer(server, dbConnection);
-        actionResult(true);
+        ResultCallback(true);
       }
       else{
         console.log("Server already exists in database.");
-        actionResult(false);
+        ResultCallback(false);
+      }
+    });
+  },
+
+  OnAddUser: function(serverID, userID, dbConnection, ResultCallback){
+    // Get server FK ID. If server does not exist, FK will be -1
+    GetServerFK(serverID, dbConnection, FK => {
+      if(FK >= 0){
+        DoesUserExistInDB(userID, serverID, dbConnection, userExists => {
+          if(userExists === true){
+            ResultCallback("User already exists");
+          }
+          else{
+            AddUserToDB(userID, FK, dbConnection);
+            ResultCallback("OK"); 
+          }
+        });
+      }
+      else{
+        console.log("Tried accessing server which does not exist in database.");
+        ResultCallback("Server not found");
       }
     });
   }
@@ -46,6 +69,8 @@ function InsertServer(server, dbConnection){
 }
 
 function InsertServerOwner(serverOwner, serverTableID, dbConnection){
+  // Might do user insert inside AddUser function, but it needs a callback.
+  // I am leaving it as is for now
   var sqlInsertServConf =  "INSERT INTO `users` (`id`, `name`, `dscr_id`, `server_fk`) VALUES " +
   "(NULL, '" + serverOwner.user.tag + "', '" + serverOwner.id + "', '" + serverTableID + "');";
   dbConnection.query(sqlInsertServConf, function (err_ins_usr, result_ins_usr) {
@@ -68,3 +93,42 @@ function InsertServerOwner(serverOwner, serverTableID, dbConnection){
     });
   });
 }
+
+function GetServerFK(serverID, dbConnection, CallbackFK){
+  var sqlServerID = "SELECT `id` FROM `servers` WHERE `dscr_id` = " + serverID;
+  dbConnection.query(sqlServerID, function (err, result, fields) {
+    if (err) throw err;
+    if(result != ""){
+      CallbackFK(result[0].id);
+    }
+    else{
+      CallbackFK(-1);
+    }
+  });
+  
+
+}
+
+function DoesUserExistInDB(userID, serverID, dbConnection, ExistsCallback){
+  var sql = "SELECT `users`.`id` FROM `users`, `servers` WHERE `servers`.`dscr_id` = " + serverID + 
+  " AND `users`.`server_fk` = `servers`.`id` AND `users`.`dscr_id` = " + userID;
+  dbConnection.query(sql, function (err, result, fields) {
+    if (err) throw err;
+    if(result == ""){
+      ExistsCallback(false);
+    }
+    else{
+      ExistsCallback(true);
+    }
+  });
+}
+
+function AddUserToDB(userID, serverID, dbConnection){
+  var sqlInsertUser =  "INSERT INTO `users` (`id`, `name`, `dscr_id`, `server_fk`) VALUES " +
+  "(NULL, '" + userID + "', '" + userID + "', '" + serverID + "');";
+  dbConnection.query(sqlInsertUser, function (error, result) {
+    if (error) throw error;
+    console.log("New user was added to users DB.");
+  });
+}
+
