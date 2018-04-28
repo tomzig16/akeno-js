0e9a565d74f78cf98484db873fb7fba3fcfebb5b
Tomas Zigmantavičius <tzigmantavicius@gmail.com>
2018-04-28 17:37:28 +0300

    Add showing image template

--- scripts/image_system.js
+++ scripts/image_system.js
@@ -15,6 +15,27 @@ module.exports = {
     if(args[0] === "add"){
       
     }
+    else if(args[0] === "show"){
+      let testingImageURL = "https://i.imgur.com/Di3z1Mc.gif";
+      message.channel.send({
+        content: "*posted by " + message.author.username + "*",
+        embed: {
+          color: message.guild.me.displayColor,
+          author: {
+            name: message.client.user.username,
+            icon_url: message.client.avatarURL
+          },
+          title: "writtenCommandHere",
+          url: testingImageURL,
+          image: {
+            url: testingImageURL,
+          },
+          thumbnail: {
+            url: message.author.avatarURL
+          }
+        }
+      });
+    }
   }
 }
 
@@ -25,10 +46,10 @@ function ImageHelp(type){
   }
   else if(type === "add"){
     helpMessage = "This command adds an image to my database so you later can simply access it by just giving me its title!\n";
-    helpMessage += "Command format: !image add [title] [image url]\n";
-    helpMessage += "I expect you to write image URL as last parameter so I can parse as long and as many words containing title as you wish ^.^\n";
+    helpMessage += "Command format: `!image add [title] [image url]`\n";
+    helpMessage += "I expect you to write direct image URL (should finish with .png, .jpg, .gif) as last parameter so I can parse as long and as many words containing title as you wish 😊\n";
     helpMessage += "If you think that you made a mistake, you can always remove your image (`!image remove help`) and add a new one!\n";
-    helpMessage += "\nAh, and yes, you can not add an image which will have \"help\" as first word in your title :P. In case you were wondering...";
+    helpMessage += "\nAh, and yes, you can not add an image which will start with \"help\", \"http://\", \"https://\" or \"www.\" in your title :P. In case you were wondering...";
   }
   else{
     return "sorry, but I know nothing about this command. Please, check `!image help` :c";
