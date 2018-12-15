22837649e487a22fa3bce27a052481f358df0b78
Tomas Zigmantavičius <tzigmantavicius@gmail.com>
2018-12-15 23:46:34 +0200

    Add new way of adding new commands

---
+++ commands/test2.js
@@ -0,0 +1,6 @@
+const Discord = require('discord.js')
+const bot = require('../index.js')
+
+bot.AddNewCommand("test", "default", "Usage description", "Full description", (message) => {
+    // message here
+});
--- index.js
+++ index.js
@@ -1,17 +1,40 @@
 const Discord = require("discord.js");
-const dotenv = require("doenv").config();
+const dotenv = require("dotenv").config();
 const mysql = require("mysql");
+const fs = require("fs");
 
-const serverControl = require("./scripts/db_controller.js");
-const honoringSystem = require("./scripts/honoring_system.js");
-const guildControls = require("./scripts/guild_controls.js");
-const imageSystem = require("./scripts/image_system.js");
+//const serverControl = require("./scripts/db_controller.js");
+//const honoringSystem = require("./scripts/honoring_system.js");
+//const guildControls = require("./scripts/guild_controls.js");
+//const imageSystem = require("./scripts/image_system.js");
 
 const defaultPrefix = "!";
 
 // Start the systems
 const client = new Discord.Client();
-client.login(process.env.BOT_TOKEN);
+
+
+registeredCommands = {
+    admin: {},
+    moderator: {},
+    default: {}
+}
+
+AddNewCommand = (command, type, cmdUsage, cmdDescription, callback) => {
+    registeredCommands[type][command] = {};
+    registeredCommands[type][command]["usage"] = cmdUsage;
+    registeredCommands[type][command]["description"] = cmdDescription;
+    registeredCommands[type][command]["execute"] = callback;
+};
+module.exports.AddNewCommand = AddNewCommand;
+
+var commands = fs.readdirSync("./commands/");
+commands.forEach((script) => {
+    if (script.substring(script.length - 3, script.length) === '.js') {
+        var req = require('./commands/' + script)
+    }
+});
+
 
 client.on("ready", () => {
     console.log("Bot has been started");
@@ -34,9 +57,11 @@ client.on("guildCreate", (guild) => {
   
   
 client.on("guildDelete", (guild) => {
+    // TODO probably should print more information about the server exited
     console.log("Removed from a guild");
 });
 
 // TODO handle errors properly (should log them somewhere)
 client.on("error", (error) => console.log("Error was encountered. Continuing..."));
   
+//client.login(process.env.BOT_TOKEN);
