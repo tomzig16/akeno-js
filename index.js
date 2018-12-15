05c4ceb0501935eef2e6a322af8282eea2085c02
Tomas Zigmantavičius <tzigmantavicius@gmail.com>
2018-12-15 21:12:52 +0200

    Start rewriting index.js

--- index.js
+++ index.js
@@ -1,91 +1,42 @@
-let serverControl = require('./scripts/db_controller.js');
-const Discord = require('discord.js');
+const Discord = require("discord.js");
+const dotenv = require("doenv").config();
+const mysql = require("mysql");
 
-var honoringSystem = require('./scripts/honoring_system.js');
-var guildControls = require('./scripts/guild_controls.js');
-var imageSystem = require('./scripts/image_system.js');
+const serverControl = require("./scripts/db_controller.js");
+const honoringSystem = require("./scripts/honoring_system.js");
+const guildControls = require("./scripts/guild_controls.js");
+const imageSystem = require("./scripts/image_system.js");
 
 const defaultPrefix = "!";
 
-
-// Connecting Discord bot
+// Start the systems
 const client = new Discord.Client();
-const token = process.env.BOT_TOKEN;
-client.login(token);
+client.login(process.env.BOT_TOKEN);
 
-client.on('ready', function() {
-  console.log('Bot has been started');
-  serverControl.StartServerPokingRoutine();
-  console.log("Database started.");
-  guildControls.LoadGuilds();
+client.on("ready", () => {
+    console.log("Bot has been started");
 });
 
+client.on("message", async (message) => {
+    if(message.author.bot) return;
+    if(message.content.indexOf(defaultPrefix) !== 0) return;
+  
+    const args = message.content.slice(defaultPrefix.length).trim().split(/ +/);
+    const command = args.shift().toLowerCase();
 
-// Bot commands
-client.on("message", async message => {
-  if(message.author.bot) return;
-  if(message.content.indexOf(defaultPrefix) !== 0) return;
-
-  const args = message.content.slice(defaultPrefix.length).trim().split(/ +/);
-  const command = args.shift().toLowerCase();
-
-  if (command === "addserver") {
-    guildControls.CMD_AddServer(message);
-    return;
-  }
-  else if (command === "akeno-mng"){
-    guildControls.CMD_ManageAkeno(message, args);
-    return;
-  }
-  // Honoring system commands
-  if(guildControls.IsFeatureEnabled("honors", message.guild.id)){
-    if(command === "join_h"){
-      honoringSystem.CMD_JoinH(message);
-      return;
-    }
-    else if(command === "status"){
-      honoringSystem.CMD_Status(message);
-      return;
-    }
-    else if(command === "collect"){
-      honoringSystem.CMD_Collect(message);
-      return;
-    }
-    else if(command === "pat"){
-      honoringSystem.CMD_Honor(message, args, "pat");
-      return;
-    }
-    else if(command === "thank"){
-      honoringSystem.CMD_Honor(message, args, "thank");
-      return;
-    }
-    else if(command === "honor"){
-      honoringSystem.CMD_Honor(message, args, "honor");
-      return;
-    }
-  }
-  if(guildControls.IsFeatureEnabled("images", message.guild.id)){
-    if(command === "image"){
-      imageSystem.ParseParameters(message, args);
-      return;
-    }
-  }
-  // Decided to ignore any unknown command
-  // in case people are trying to call another bot
-  /*else{
-    message.reply("sorry, I don't really understand you. `!help` will tell you more!");
-  }*/
-
+    // TODO Execute commands
 });
 
-
-client.on("guildCreate", guild => {
-  console.log("New guild joined");
+client.on("guildCreate", (guild) => {
+    // TODO probably should print more information about the server joined
+    console.log("New guild joined");
+});
+  
+  
+client.on("guildDelete", (guild) => {
+    console.log("Removed from a guild");
 });
 
-
-client.on("guildDelete", guild => {
-  console.log("Removed from a guild");
-});
-
+// TODO handle errors properly (should log them somewhere)
 client.on("error", (error) => console.log("Error was encountered. Continuing..."));
+  
